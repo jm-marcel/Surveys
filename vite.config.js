0501a5717ffde742bd7c5bca81517dcc0180d8ef
JMarcel_MI <jmarcel@redemidiainterativa.com.br>
2023-01-13 18:02:32 -0300

    feat: :tada: Laravue Project XD
    Laravel + Sail + Vite + Vue Project

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+
+export default defineConfig({
+    server: {
+        host: true,
+        hmr: {
+            host: 'localhost',
+            overlay: false
+        }
+    },
+    plugins: [
+        laravel({
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js'
+            ],
+            refresh: true
+        })
+    ],
+});
